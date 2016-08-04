1034ac23b1d43ba151dab971a4a267b772ce7a4e
Emmanuel Drouet <emmanuel.drouet@gmail.com>
2016-08-04 12:55:09 +0200

    initialize SiteBuilderBundle

--- DependencyInjection/EdgarEzSiteBuilderExtension.php
+++ DependencyInjection/EdgarEzSiteBuilderExtension.php
@@ -2,17 +2,20 @@
 
 namespace EdgarEz\SiteBuilderBundle\DependencyInjection;
 
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class EdgarEzSiteBuilderExtension extends Extension
+class EdgarEzSiteBuilderExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -24,5 +27,24 @@ class EdgarEzSiteBuilderExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $container->prependExtensionConfig('assetic', array('bundles' => array('EdgarEzSiteBuilderBundle')));
+
+        $this->prependYui($container);
+    }
+
+    private function prependYui(ContainerBuilder $container)
+    {
+        $container->setParameter(
+            'edgar_ez_sb.public_dir',
+            'bundles/edgarezsitebuilder'
+        );
+        $yuiConfigFile = __DIR__ . '/../Resources/config/yui.yml';
+        $config = Yaml::parse(file_get_contents($yuiConfigFile));
+        $container->prependExtensionConfig('ez_platformui', $config);
+        $container->addResource(new FileResource($yuiConfigFile));
     }
 }
