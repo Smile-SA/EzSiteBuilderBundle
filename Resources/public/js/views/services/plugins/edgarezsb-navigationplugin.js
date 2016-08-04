6bbba62c6a5c60b8c7fc987f900f187466fa1df7
Emmanuel Drouet <emmanuel.drouet@gmail.com>
2016-08-04 03:57:50 -0700

    add platform site builder navigation

---
+++ Resources/public/js/views/services/plugins/edgarezsb-navigationplugin.js
@@ -0,0 +1,26 @@
+YUI.add('edgarezsb-navigationplugin', function (Y) {
+    Y.namespace('edgarEzSb.Plugin');
+
+    Y.edgarEzSb.Plugin.NavigationPlugin = Y.Base.create('edgarezsbNavigationPlugin', Y.eZ.Plugin.ViewServiceBase, [], {
+        initializer: function () {
+            var service = this.get('host'); // the plugged object is called host
+
+            service.addNavigationItem({
+                Constructor: Y.eZ.NavigationItemView,
+                config: {
+                    title: "Site Builder",
+                    identifier: "edgarezsb-list-contents",
+                    route: {
+                        name: "edgarEzSbDashboard"
+                    }
+                }
+            }, 'platform');
+        },
+    }, {
+        NS: 'edgarezsbNavigation'
+    });
+
+    Y.eZ.PluginRegistry.registerPlugin(
+        Y.edgarEzSb.Plugin.NavigationPlugin, ['navigationHubViewService']
+    );
+});
