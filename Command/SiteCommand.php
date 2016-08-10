89b086669c35bed0560bdae88f21afdabe8d3121
Emmanuel Drouet <emmanuel.drouet@gmail.com>
2016-08-10 05:10:05 -0700

    initialize site generator command

---
+++ Command/SiteCommand.php
@@ -0,0 +1,269 @@
+<?php
+
+namespace EdgarEz\SiteBuilderBundle\Command;
+
+use EdgarEz\SiteBuilderBundle\Generator\CustomerGenerator;
+use EdgarEz\SiteBuilderBundle\Generator\ProjectGenerator;
+use EdgarEz\SiteBuilderBundle\Generator\SiteGenerator;
+use EdgarEz\ToolsBundle\Service\Content;
+use eZ\Publish\API\Repository\Exceptions\InvalidArgumentException;
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
+use eZ\Publish\API\Repository\Exceptions\UnauthorizedException;
+use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\API\Repository\Repository;
+use eZ\Publish\API\Repository\URLAliasService;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+
+/**
+ * Class SiteCommand
+ *
+ * @package EdgarEz\SiteBuilderBundle\Command
+ */
+class SiteCommand extends BaseContainerAwareCommand
+{
+    /**
+     * @var int $siteLocationID site root location ID
+     */
+    protected $siteLocationID;
+
+    /**
+     * @var string $vendorName namespace vendor name where project sitebuilder will be generated
+     */
+    protected $vendorName;
+
+    /**
+     * @var string $customerName customer name
+     */
+    protected $customerName;
+
+    /**
+     * @var string $modelName model name
+     */
+    protected $modelName;
+
+    /** @var string $siteName site name */
+    protected $siteName;
+
+    /**
+     * @var string $excludeUriPrefixes ezplatform path prefix
+     */
+    protected $excludeUriPrefixes;
+
+    /**
+     * @var string $dir system directory where bundle would be generated
+     */
+    protected $dir;
+
+    /**
+     * Configure Customer generator command
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('edgarez:sitebuilder:site:generate')
+            ->setDescription('Generate SiteBuilder Site (Content Structure and Bundle)');
+    }
+
+    /**
+     * Execute command
+     *
+     * @param InputInterface  $input input console
+     * @param OutputInterface $output output console
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $questionHelper = $this->getQuestionHelper();
+        $questionHelper->writeSection($output, 'SiteBuilder Site initialization');
+
+        $this->createSiteContent($input, $output);
+        $this->createSiteBundle($input, $output);
+
+        /** @var SiteGenerator $generator */
+        $generator = $this->getGenerator();
+        $generator->generate(
+            $this->siteLocationID,
+            $this->vendorName,
+            $this->customerName,
+            $this->modelName,
+            $this->siteName,
+            $this->excludeUriPrefixes,
+            $this->dir
+        );
+
+        $namespace = $this->vendorName . '\\' . ProjectGenerator::CUSTOMERS . '\\' . $this->customerName . '\\' . CustomerGenerator::SITES . '\\' . $this->siteName . 'Bundle';
+        $bundle = $this->vendorName . ProjectGenerator::CUSTOMERS . $this->customerName . CustomerGenerator::SITES . $this->siteName . 'Bundle';
+        $this->updateKernel($questionHelper, $input, $output, $this->getContainer()->get('kernel'), $namespace, $bundle);
+
+        $output->writeln(array(
+            '',
+            $this->getHelper('formatter')->formatBlock('SiteBuilder Contents and Structure generated', 'bg=blue;fg=white', true),
+            ''
+        ));
+    }
+
+    /**
+     * Choose and copy Model content structure to customer content tree
+     *
+     * @param InputInterface  $input input console
+     * @param OutputInterface $output output console
+     */
+    protected function createSiteContent(InputInterface $input, OutputInterface $output)
+    {
+        $questionHelper = $this->getQuestionHelper();
+
+        $vendorName = false;
+        $question = new Question($questionHelper->getQuestion('Project Vendor name used to construct namespace', null));
+        $question->setValidator(
+            array(
+                'EdgarEz\SiteBuilderBundle\Command\Validators',
+                'validateVendorName'
+            )
+        );
+
+        while (!$vendorName) {
+            $vendorName = $questionHelper->ask($input, $output, $question);
+        }
+
+        $this->vendorName = $vendorName;
+
+        $siteName = false;
+        $question = new Question($questionHelper->getQuestion('Site name you want to create', null));
+        $question->setValidator(
+            array(
+                'EdgarEz\SiteBuilderBundle\Command\Validators',
+                'validateVendorName'
+            )
+        );
+
+        while (!$siteName) {
+            $siteName = $questionHelper->ask($input, $output, $question);
+        }
+
+        $this->siteName = $siteName;
+
+        /** @var Repository $repository */
+        $repository = $this->getContainer()->get('ezpublish.api.repository');
+
+        /** @var LocationService $locationService */
+        $locationService = $repository->getLocationService();
+        /** @var URLAliasService $urlAliasService */
+        $urlAliasService = $repository->getURLAliasService();
+
+        // Get customer content root location ID
+        $customerLocationID = false;
+        $question = new Question($questionHelper->getQuestion('Customer location ID yout where site content would be generated', $customerLocationID));
+        $question->setValidator(
+            array(
+                'EdgarEz\SiteBuilderBundle\Command\Validators',
+                'validateLocationID'
+            )
+        );
+
+        while (!$customerLocationID) {
+            $customerLocationID = $questionHelper->ask($input, $output, $question);
+
+            try {
+                $locationService->loadLocation($customerLocationID);
+                if (!$customerLocationID || empty($customerLocationID)) {
+                    $output->writeln("<error>Customer Location ID is not valid</error>");
+                }
+            } catch (NotFoundException $e) {
+                $output->writeln("<error>No location found with id $customerLocationID</error>");
+                $customerLocationID = false;
+            }
+        }
+
+        // Get model content root location ID
+        $modelLocationID = false;
+        $question = new Question($questionHelper->getQuestion('Model location ID yout want to use to generate ste content', $modelLocationID));
+        $question->setValidator(
+            array(
+                'EdgarEz\SiteBuilderBundle\Command\Validators',
+                'validateLocationID'
+            )
+        );
+
+        while (!$modelLocationID) {
+            $modelLocationID = $questionHelper->ask($input, $output, $question);
+
+            try {
+                $locationService->loadLocation($modelLocationID);
+                if (!$modelLocationID || empty($modelLocationID)) {
+                    $output->writeln("<error>Model Location ID is not valid</error>");
+                }
+            } catch (NotFoundException $e) {
+                $output->writeln("<error>No location found with id $modelLocationID</error>");
+                $modelLocationID = false;
+            }
+        }
+
+        $customerLocation = $locationService->loadLocation($customerLocationID);
+        $this->customerName = $customerLocation->getContentInfo()->name;
+        $modelLocation = $locationService->loadLocation($modelLocationID);
+        $this->modelName = $modelLocation->getContentInfo()->name;
+
+        try {
+            // Copy model content subtree to customer tree
+            /** @var Content $content */
+            $content = $this->getContainer()->get('edgar_ez_tools.content.service');
+            $siteLocationID = $content->copySubtree($modelLocationID, $customerLocationID, $this->siteName);
+
+            $this->siteLocationID = $siteLocationID;
+            $newLocation = $locationService->loadLocation($this->siteLocationID);
+
+            $contentPath = $urlAliasService->reverseLookup($newLocation, $newLocation->getContentInfo()->mainLanguageCode)->path;
+            $this->excludeUriPrefixes = trim($contentPath, '/') . '/';
+        } catch (InvalidArgumentException $e) {
+            $output->writeln("<error>Invalid argument [$modelLocationID] or [$customerLocationID]</error>");
+        }
+    }
+
+    /**
+     * Retrieve informations to generate new Customer site bundle
+     *
+     * @param InputInterface  $input input console
+     * @param OutputInterface $output output console
+     */
+    protected function createSiteBundle(InputInterface $input, OutputInterface $output)
+    {
+        $questionHelper = $this->getQuestionHelper();
+
+        $dir = false;
+        while (!$dir) {
+            $dir = dirname($this->getContainer()->getParameter('kernel.root_dir')).'/src';
+
+            $output->writeln(array(
+                '',
+                'The bundle can be generated anywhere. The suggested default directory uses',
+                'the standard conventions.',
+                '',
+            ));
+
+            $question = new Question($questionHelper->getQuestion('Target directory', $dir), $dir);
+            $question->setValidator(
+                array(
+                    'EdgarEz\SiteBuilderBundle\Command\Validators',
+                    'validateTargetDir'
+                )
+            );
+            $dir = $questionHelper->ask($input, $output, $question);
+        }
+
+        $this->dir = $dir;
+    }
+
+    /**
+     * Initialize customer generator tool
+     *
+     * @return SiteGenerator customer generator tool
+     */
+    protected function createGenerator()
+    {
+        return new SiteGenerator(
+            $this->getContainer()->get('filesystem'),
+            $this->getContainer()->get('kernel')
+        );
+    }
+}
---
+++ Generator/SiteGenerator.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace EdgarEz\SiteBuilderBundle\Generator;
+
+use Sensio\Bundle\GeneratorBundle\Generator\Generator;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpKernel\Kernel;
+
+/**
+ * Class SiteGenerator
+ *
+ * @package EdgarEz\SiteBuilderBundle\Generator
+ */
+class SiteGenerator extends Generator
+{
+    /**
+     * @var Filesystem $filesystem
+     */
+    private $filesystem;
+
+    /**
+     * @var Kernel $kernel
+     */
+    private $kernel;
+
+    /**
+     * SiteGenerator constructor.
+     *
+     * @param Filesystem $filesystem
+     * @param Kernel     $kernel
+     */
+    public function __construct(Filesystem $filesystem, Kernel $kernel)
+    {
+        $this->filesystem = $filesystem;
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * Generate Customer Bundle
+     *
+     * @param int $siteLocationID customer site content location ID
+     * @param string $vendorName project vendor name
+     * @param string $siteName customer site name
+     * @param string $targetDir filesystem directory where bundle would be generated
+     */
+    public function generate($siteLocationID, $vendorName, $customerName, $modelName, $siteName, $excludeUriPrefixes, $targetDir)
+    {
+        $namespace = $vendorName . '\\' . ProjectGenerator::CUSTOMERS . '\\' . $customerName . '\\' . CustomerGenerator::SITES . '\\' . $siteName . 'Bundle';
+
+        $dir = $targetDir . '/' . strtr($namespace, '\\', '/');
+        if (file_exists($dir)) {
+            if (!is_dir($dir)) {
+                throw new \RuntimeException(sprintf('Unable to generate the bundle as the target directory "%s" exists but is a file.', realpath($dir)));
+            }
+            $files = scandir($dir);
+            if ($files != array('.', '..')) {
+                throw new \RuntimeException(sprintf('Unable to generate the bundle as the target directory "%s" is not empty.', realpath($dir)));
+            }
+            if (!is_writable($dir)) {
+                throw new \RuntimeException(sprintf('Unable to generate the bundle as the target directory "%s" is not writable.', realpath($dir)));
+            }
+        }
+
+        $basename = substr($vendorName . ProjectGenerator::CUSTOMERS . $customerName . CustomerGenerator::SITES . $siteName . 'Bundle', 0, -6);
+        $parameters = array(
+            'namespace' => $namespace,
+            'bundle'    => $siteName . 'Bundle',
+            'format'    => 'yml',
+            'bundle_basename' => $basename,
+            'extension_alias' => Container::underscore($basename),
+            'vendor_name' => $vendorName,
+            'customer_name' => $customerName,
+            'model_name' => $modelName,
+            'site_name' => $siteName,
+            'siteLocationID' => $siteLocationID,
+            'parent_model_bundle' => substr($vendorName . ProjectGenerator::MODELS . $modelName . 'Bundle', 0, -6),
+            'siteaccess_model' => Container::underscore($vendorName . $modelName),
+            'siteaccess' => Container::underscore($vendorName . $customerName . $siteName),
+            'host' => 'ezplatform.lxc',
+            'exclude_uri_prefixes' => $excludeUriPrefixes
+        );
+
+        $this->setSkeletonDirs(array($this->kernel->locateResource('@EdgarEzSiteBuilderBundle/Resources/skeleton')));
+        $this->renderFile('site/Bundle.php.twig', $dir . '/' . $basename . 'Bundle.php', $parameters);
+        $this->renderFile('site/Extension.php.twig', $dir . '/DependencyInjection/' . $basename . 'Extension.php', $parameters);
+        $this->renderFile('site/Configuration.php.twig', $dir . '/DependencyInjection/Configuration.php', $parameters);
+        $this->renderFile('site/Resources/config/ezplatform.yml.twig', $dir . '/Resources/config/ezplatform.yml', $parameters);
+    }
+}
