d16d97e95a2125ba0bf7b65fe01baaad2d970bbf
Emmanuel Drouet <emmanuel.drouet@gmail.com>
2016-08-27 11:25:24 -0700

    move security check to service

---
+++ Service/SecurityService.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace EdgarEz\SiteBuilderBundle\Service;
+
+use eZ\Publish\API\Repository\RoleService;
+use eZ\Publish\API\Repository\Values\User\Policy;
+use eZ\Publish\Core\MVC\Symfony\Security\Authorization\Attribute;
+use eZ\Publish\Core\MVC\Symfony\Security\User;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+
+class SecurityService
+{
+    /** @var TokenStorage $tokenStorage */
+    protected $tokenStorage;
+
+    /** @var AuthorizationChecker $authorizationChecker */
+    protected $authorizationChecker;
+
+    /** @var RoleService $roleService */
+    protected $roleService;
+
+    public function __construct(
+        TokenStorage $tokenStorage,
+        AuthorizationChecker $authorizationChecker,
+        RoleService $roleService
+    )
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->authorizationChecker = $authorizationChecker;
+        $this->roleService = $roleService;
+    }
+
+    public function checkAuthorization($func)
+    {
+        if (!$this->authorizationChecker->isGranted(new Attribute('sitebuilder', $func))) {
+            return false;
+        }
+
+        /**
+         * Users with policies module *, function * will not have access
+         * to functions sitegenerate and siteactivate from sitebuilder module.
+         */
+        if ($func == 'sitegenerate' || $func == 'siteactivate') {
+            /** @var User $user */
+            $user = $this->tokenStorage->getToken()->getUser();
+            /** @var Policy[] $policies */
+            $policies = $this->roleService->loadPoliciesByUserId($user->getAPIUser()->id);
+            foreach ($policies as $policy) {
+                if ($policy->module == '*' && $policy->function == '*') {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
--- Twig/Extension/SecurityExtension.php
+++ Twig/Extension/SecurityExtension.php
@@ -2,33 +2,16 @@
 
 namespace EdgarEz\SiteBuilderBundle\Twig\Extension;
 
-use eZ\Publish\API\Repository\RoleService;
-use eZ\Publish\API\Repository\Values\User\Policy;
-use eZ\Publish\Core\MVC\Symfony\Security\Authorization\Attribute;
-use eZ\Publish\Core\MVC\Symfony\Security\User;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
-use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+use EdgarEz\SiteBuilderBundle\Service\SecurityService;
 
 class SecurityExtension extends \Twig_Extension
 {
-    /** @var TokenStorage $tokenStorage */
-    protected $tokenStorage;
+    /** @var SecurityService $securityService */
+    protected $securityService;
 
-    /** @var AuthorizationChecker $authorizationChecker */
-    protected $authorizationChecker;
-
-    /** @var RoleService $roleService */
-    protected $roleService;
-
-    public function __construct(
-        TokenStorage $tokenStorage,
-        AuthorizationChecker $authorizationChecker,
-        RoleService $roleService
-    )
+    public function __construct(SecurityService $securityService)
     {
-        $this->tokenStorage = $tokenStorage;
-        $this->authorizationChecker = $authorizationChecker;
-        $this->roleService = $roleService;
+        $this->securityService = $securityService;
     }
 
     /**
@@ -54,26 +37,6 @@ class SecurityExtension extends \Twig_Extension
 
     public function checkAuthorization($func)
     {
-        if (!$this->authorizationChecker->isGranted(new Attribute('sitebuilder', $func))) {
-            return false;
-        }
-
-        /**
-         * Users with policies module *, function * will not have access
-         * to functions sitegenerate and siteactivate from sitebuilder module.
-         */
-        if ($func == 'sitegenerate' || $func == 'siteactivate') {
-            /** @var User $user */
-            $user = $this->tokenStorage->getToken()->getUser();
-            /** @var Policy[] $policies */
-            $policies = $this->roleService->loadPoliciesByUserId($user->getAPIUser()->id);
-            foreach ($policies as $policy) {
-                if ($policy->module == '*' && $policy->function == '*') {
-                    return false;
-                }
-            }
-        }
-
-        return true;
+        return $this->securityService->checkAuthorization($func);
     }
 }
