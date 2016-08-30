66a265020fe7043d86b7417075b9007812c2db36
Emmanuel Drouet <emmanuel.drouet@gmail.com>
2016-08-30 12:21:39 -0700

    fix constraint messages

--- Form/Validator/Constraint/VendorNameConstraint.php
+++ Form/Validator/Constraint/VendorNameConstraint.php
@@ -7,7 +7,7 @@ use Symfony\Component\Validator\Constraint;
 class VendorNameConstraint extends Constraint
 {
     public $message = 'The string "%string%" contains an illegal character: it can only contain letters ' .
-    'and should start with uppercase.';
+    'uppercase first.';
 
     public function validatedBy()
     {
