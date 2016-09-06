f384aa52e4f6e54d97a9b5a9f45b1cc7c472093f
Emmanuel Drouet <emmanuel.drouet@gmail.com>
2016-09-05 23:52:35 -0700

    dont display site generate form when no model available

--- Controller/SbController.php
+++ Controller/SbController.php
@@ -9,6 +9,9 @@ use EdgarEz\SiteBuilderBundle\Form\Type\SiteType;
 use EdgarEz\SiteBuilderBundle\Generator\CustomerGenerator;
 use EdgarEz\SiteBuilderBundle\Generator\ProjectGenerator;
 use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\API\Repository\SearchService;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\Search\SearchResult;
 use eZ\Publish\API\Repository\Values\User\User;
 use EzSystems\PlatformUIBundle\Controller\Controller;
 
@@ -17,11 +20,18 @@ class SbController extends Controller
     /** @var LocationService $locationService */
     protected $locationService;
 
+    /** @var SearchService $searchService */
+    protected $searchService;
+
     protected $tabItems;
 
-    public function __construct(LocationService $locationService, $tabItems)
+    public function __construct(
+        LocationService $locationService,
+        SearchService $searchService,
+        $tabItems)
     {
         $this->locationService = $locationService;
+        $this->searchService = $searchService;
         $this->tabItems = $tabItems;
     }
 
@@ -112,28 +122,35 @@ class SbController extends Controller
     {
         if (isset($paramsTwig['sitegenerate'])) {
             $params['siteForm'] = $paramsTwig['sitegenerate'];
-        } else {
-            $customerName = $this->getCustomerName();
-
-            $customerAlias = strtolower(
-                ProjectGenerator::CUSTOMERS . '_' . $customerName . '_' . CustomerGenerator::SITES
-            );
-            $params['siteForm'] = $this->createForm(
-                new SiteType(
-                    $this->container->get('ezpublish.api.service.location'),
-                    $this->container->get('ezpublish.api.service.search'),
-                    $this->container->getParameter('edgarez_sb.project.default.models_location_id'),
-                    $this->container->getParameter('edgarez_sb.project.default.media_models_location_id'),
-                    $this->container->getParameter(
-                        'edgarez_sb.customer.' . $customerAlias . '.default.customer_location_id'
-                    ),
-                    $this->container->getParameter(
-                        'edgarez_sb.customer.' . $customerAlias . '.default.media_customer_location_id'
-                    ),
-                    $customerName
-                )
-            )->createView();
+            return $params;
         }
+
+        $countActiveModels = $this->getCountActiveModels();
+        if (!$countActiveModels) {
+            $params['siteForm'] = false;
+            return $params;
+        }
+
+        $customerName = $this->getCustomerName();
+
+        $customerAlias = strtolower(
+            ProjectGenerator::CUSTOMERS . '_' . $customerName . '_' . CustomerGenerator::SITES
+        );
+        $params['siteForm'] = $this->createForm(
+            new SiteType(
+                $this->container->get('ezpublish.api.service.location'),
+                $this->container->get('ezpublish.api.service.search'),
+                $this->container->getParameter('edgarez_sb.project.default.models_location_id'),
+                $this->container->getParameter('edgarez_sb.project.default.media_models_location_id'),
+                $this->container->getParameter(
+                    'edgarez_sb.customer.' . $customerAlias . '.default.customer_location_id'
+                ),
+                $this->container->getParameter(
+                    'edgarez_sb.customer.' . $customerAlias . '.default.media_customer_location_id'
+                ),
+                $customerName
+            )
+        )->createView();
 
         return $params;
     }
@@ -151,5 +168,23 @@ class SbController extends Controller
 
         $parent = $this->locationService->loadLocation($userLocation->parentLocationId);
         return $parent->contentInfo->name;
+    }
+
+    protected function getCountActiveModels()
+    {
+        $query = new Query();
+        $locationCriterion = new Query\Criterion\ParentLocationId(
+            $this->container->getParameter('edgarez_sb.project.default.models_location_id')
+        );
+        $contentTypeIdentifier = new Query\Criterion\ContentTypeIdentifier('edgar_ez_sb_model');
+        $activated = new Query\Criterion\Field('activated', Query\Criterion\Operator::EQ, true);
+
+        $query->filter = new Query\Criterion\LogicalAnd(
+            array($locationCriterion, $contentTypeIdentifier, $activated)
+        );
+
+        /** @var SearchResult $result */
+        $result = $this->searchService->findContent($query);
+        return $result->totalCount;
     }
 }
